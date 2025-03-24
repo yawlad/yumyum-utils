c30dc2f3e0528f76bf08292ce85c018be331b8b0
y.yawlad <y.yawlad@gmail.com>
2025-03-24 19:34:17 +0300

    fixed offsets

--- components/PriceTags/PriceTag.tsx
+++ components/PriceTags/PriceTag.tsx
@@ -6,10 +6,10 @@ interface PriceTagProps {
 export default function PriceTag({ name, price }: PriceTagProps) {
   return (
     <div className="box-border w-[74.25mm] h-[52.5mm] p-2 border flex flex-col items-center justify-center relative bg-price bg-contain bg-center">
-      <div className="absolute leading-none top-[22%] left-1/2 -translate-x-1/2 text-[22px] font-bold text-pink w-[75%] text-center">
+      <div className="absolute leading-none top-[40%] left-1/2 -translate-x-1/2 -translate-y-1/2 text-[22px] font-bold text-pink w-[70%] text-center">
         {name}
       </div>
-      <div className="absolute bottom-[11%] left-1/2 -translate-x-1/2 -translate-y-1/2 text-[28px] font-extrabold text-orange font-rotondac">
+      <div className="absolute bottom-[10%] left-1/2 -translate-x-1/2 -translate-y-1/2 text-[26px] font-extrabold text-orange font-rotondac">
         {price.toFixed(2)}
       </div>
       <div className="absolute bottom-[0.7mm] right-[1mm] text-[10px] text-white">
--- components/PriceTags/PriceTagDiscount.tsx
+++ components/PriceTags/PriceTagDiscount.tsx
@@ -11,10 +11,10 @@ export default function PriceTagDiscount({
 }: PriceTagDiscountProps) {
   return (
     <div className="box-border w-[74.25mm] h-[52.5mm] p-2 border flex flex-col items-center justify-center relative bg-price-discount bg-contain bg-center">
-      <div className="absolute leading-none top-[22%] left-1/2 -translate-x-1/2 text-[22px] font-bold text-pink w-[75%] text-center">
+      <div className="absolute leading-none top-[40%] left-1/2 -translate-x-1/2 -translate-y-1/2 text-[22px] font-bold text-pink w-[70%] text-center">
         {name}
       </div>
-      <div className="absolute bottom-[11%] left-1/2 -translate-x-1/2 -translate-y-1/2 text-[28px] font-extrabold text-orange font-rotondac">
+      <div className="flex gap-1 items-center absolute bottom-[10%] left-1/2 -translate-x-1/2 -translate-y-1/2 text-[26px] font-extrabold text-orange font-rotondac">
         <span className="text-[20px] line-through">{price.toFixed(2)}</span>
         <span className="text-[32px]">{discount.toFixed(2)}</span>
       </div>
