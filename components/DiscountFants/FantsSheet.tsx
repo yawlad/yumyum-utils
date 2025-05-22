bd251c49bca87f6d4e9197f9e1eb21f5d3d5099e
y.yawlad <y.yawlad@gmail.com>
2025-05-22 21:10:10 +0300

    fixed orange v2

--- components/DiscountFants/FantsSheet.tsx
+++ components/DiscountFants/FantsSheet.tsx
@@ -1,34 +1,21 @@
 import useMainStore from "@/stores/useMainStore"; // Adjust the import path
+import FantsTag from "./FantsTag";
 
 export default function FantsSheet() {
   const { fants } = useMainStore();
-
-  const getPrizeColor = (type: number) => {
-    switch (type) {
-      case 1:
-        return "text-green-500"; // мелкий
-      case 2:
-        return "text-orange-500"; // средний
-      case 3:
-        return "text-red-500"; // большой
-      default:
-        return "text-gray-300";
-    }
-  };
 
   return (
     <div>
       <div className="flex flex-wrap justify-center w-[297mm] mx-auto action-sheet">
         {fants.flatMap((fant: ProductForFant) =>
           Array.from({ length: fant.quantity }).map((_, i) => (
-            <div
-              key={`${fant.id}-${i}`}
-              className={`w-[20%] h-[21mm] p-2 border flex items-center justify-center text-[16px] leading-none font-bold text-center action-tag ${getPrizeColor(
-                fant.type
-              )}`}
-            >
-              {fant.name}
-            </div>
+            <FantsTag
+              name={fant.name}
+              type={fant.type}
+              index={i}
+              fantId={i}
+              key={i}
+            />
           ))
         )}
       </div>
